10daff9a07b7a53065fa60444189d439bda80521
Mahmoud Saada <mahmoud.saada@adp.com>
2016-09-26 18:59:09 -0400

    Small style changes

--- src/Cell.js
+++ src/Cell.js
@@ -5,8 +5,11 @@ class Cell extends Component {
   render () {
     return <div className="Cell">
       <div className="hex">&#x2B22;</div>
-      <label>{this.props.name}</label>
-      <div>total: {this.props.count}</div>
+      <div className="cell-text">
+        {this.props.name}
+        <br />
+        total: {this.props.count}
+      </div>
     </div>
   }
 }
