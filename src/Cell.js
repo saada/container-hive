c3d73a7ccb2b3fe70490dd4f3955f99d3c64d0cd
Mahmoud Saada <mahmoud@targetcw.com>
2016-10-01 12:22:15 -0400

    Updates

--- src/Cell.js
+++ src/Cell.js
@@ -1,22 +1,21 @@
 import React, { Component, PropTypes } from 'react'
 import './Cell.css'
+
 class Cell extends Component {
 
   render () {
     return <div className="Cell">
-      <div className="hex">&#x2B22;</div>
-      <div className="cell-text">
-        {this.props.name}
-        <br />
-        total: {this.props.count}
-      </div>
+      <div className={`hex ${this.props.isPlaceholder ? 'placeholder' : ''}`}>&#x2B22;</div>
+      <div>{this.props.name}</div>
+      <div>{this.props.count}</div>
     </div>
   }
 }
 
 Cell.propTypes = {
   name: PropTypes.string,
-  count: PropTypes.number
+  count: PropTypes.number,
+  isPlaceholder: PropTypes.string
 }
 
 export default Cell
